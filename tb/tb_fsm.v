49f742969f2d386c8e96fd75882df000d5dbc7d9
Narjis fatima <narjisfatima135@gmail.com>
2026-04-22 00:01:07 +0530

    Added FSM testbench

---
+++ tb/tb_fsm.v
@@ -0,0 +1,25 @@
+module tb_fsm;
+
+reg clk, rst, in;
+wire out;
+
+fsm uut (.clk(clk), .rst(rst), .in(in), .out(out));
+
+// clock generation
+always #5 clk = ~clk;
+
+initial begin
+clk = 0; rst = 1; in = 0;
+
+#10 rst = 0;
+
+#10 in = 1;
+#10 in = 1;
+#10 in = 0;
+#10 in = 1;
+#10 in = 1;
+
+#20 $finish;
+end
+
+endmodule 
